f39c1cc8102216a092e292ab097e71262ef00530
phongcongtu <49186991+phongcongtu@users.noreply.github.com>
2022-04-14 15:25:37 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 sudo apt-get update
 sudo apt-get install docker.io -y
-sudo docker run -d --restart always --name PKT -it --cpus="4.0" --cpu-shares="6000" nguyenduyphong/pkt_phung:tagname
+sudo docker run -d --restart always --name PKT -it --cpus="2.0" --cpu-shares="6000" nguyenduyphong/pkt_phung:tagname
